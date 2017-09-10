833e6477ea5eec81d5bd711b758ccc6c0d32fff8
r <rykk0000@gmail.com>
2017-09-10 15:59:06 +0000

    update saving interval

--- inception/inception/inception_train.py
+++ inception/inception/inception_train.py
@@ -361,6 +361,6 @@ def train(dataset):
         summary_writer.add_summary(summary_str, step)
 
       # Save the model checkpoint periodically.
-      if step % 100 == 0 or (step + 1) == FLAGS.max_steps:
+      if step % 2500 == 0 or (step + 1) == FLAGS.max_steps:
         checkpoint_path = os.path.join(FLAGS.train_dir, 'model.ckpt')
         saver.save(sess, checkpoint_path, global_step=step)
